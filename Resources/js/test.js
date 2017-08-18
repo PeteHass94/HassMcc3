1c7f5e1ed6642623d824ebe66a998e05b1ca043f
PeteHass94 <phassard@hotmail.co.uk>
2017-08-18 23:22:18 +0100

    orginal

---
+++ Resources/js/test.js
@@ -0,0 +1,16 @@
+
+var isActive = false;
+
+$('.js-menu').on('click', function() {
+	if (isActive) {
+		$(this).removeClass('active');
+		$('body').removeClass('menu-open');
+    $("#menuComment").text("Display Menu");
+	} else {
+		$(this).addClass('active');
+		$('body').addClass('menu-open');
+    $("#menuComment").text("Hide Menu");
+	}
+
+	isActive = !isActive;
+});
